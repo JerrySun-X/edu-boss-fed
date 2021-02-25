9a95032dd38fb54c98bf58c1269a9e9ebc4b19ff
Jianli Sun <740583163@qq.com>
2021-02-25 11:48:43 +0800

    基础登录模块请求提交

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -34,6 +34,8 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import request from '@/utils/request'
+import qs from 'qs'
 // import { Form } from 'element-ui'
 // import { login } from '@/services/user'
 
@@ -44,61 +46,25 @@ export default Vue.extend({
       form: {
         phone: '18201288771',
         password: '111111'
-      },
-      rules: {
-        phone: [
-          { required: true, message: '请输入手机号', trigger: 'blur' },
-          { pattern: /^1\d{10}$/, message: '请输入正确的手机号', trigger: 'blur' }
-        ],
-        password: [
-          { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
-        ]
-      },
-      isLoginLoading: false
+      }
     }
   },
 
   methods: {
-    // async onSubmit () {
-    //   try {
-    //     // 1. 表单验证
-    //     await (this.$refs.form as Form).validate()
-
-    //     // 登录按钮 loading
-    //     this.isLoginLoading = true
-
-    //     // 2. 验证通过 -> 提交表单
-    //     const { data } = await login(this.form)
-    //     // const { data } = await request({
-    //     //   method: 'POST',
-    //     //   url: '/front/user/login',
-    //     //   headers: { 'content-type': 'application/x-www-form-urlencoded' },
-    //     //   data: qs.stringify(this.form) // axios 默认发送的是 application/json 格式的数据
-    //     // })
-
-    //     // 3. 处理请求结果
-    //     //    失败：给出提示
-    //     if (data.state !== 1) {
-    //       this.$message.error(data.message)
-    //     } else {
-    //       // 1. 登录成功，记录登录状态，状态需要能够全局访问（放到 Vuex 容器中）
-    //       this.$store.commit('setUser', data.content)
-    //       // 2. 然后在访问需要登录的页面的时候判断有没有登录状态（路由拦截器）
-    //       //    成功：跳转回原来页面或首页
-    //       this.$router.push(this.$route.query.redirect as string || '/')
-    //       // this.$router.push({
-    //       //   name: 'home'
-    //       // })
-    //       this.$message.success('登录成功')
-    //     }
-    //   } catch (err) {
-    //     console.log('登录失败', err)
-    //   }
-
-    //   // 结束登录按钮的 loading
-    //   this.isLoginLoading = false
-    // }
+    async onSubmit () {
+      // 1、表单验证
+      // 2、验证通过 -> 提交表单
+      const { data } = await request({
+        method: 'POST',
+        url: '/front/user/login',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: qs.stringify(this.form) // axios 默认发送的是 application/json 格式的数据
+      })
+      console.log(data)
+      // 3、处理请求结果
+      //   成功：跳转首页
+      //   失败：给出提示
+    }
   }
 })
 </script>
